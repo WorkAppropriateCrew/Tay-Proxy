81e5a2ced5238d770825b4cf98d9ce1864e61511
Tay <taylor.mleigh@gmail.com>
2020-02-07 16:15:13 -0600

    proxy work

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require("path");
-const SRC_DIR = path.join(__dirname, "/client");
-const DIST_DIR = path.join(__dirname, "/dist");
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: DIST_DIR
   },
   module: {
     rules: [
       {
-        test: /\.js?/,
-        include: SRC_DIR,
-        loader: "babel-loader"
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          'presets': ['@babel/preset-env', '@babel/preset-react']
+        }
       }
     ]
   }

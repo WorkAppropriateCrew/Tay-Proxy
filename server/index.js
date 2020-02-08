b431d0ed1b578917652e74030c629aa978bc10ae
Tay <taylor.mleigh@gmail.com>
2020-02-08 11:58:53 -0600

    getting ready for deploy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8081;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);

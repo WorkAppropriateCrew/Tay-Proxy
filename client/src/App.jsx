4d564559c9f9fc4b87aed93db949de386f18a934
Tay <taylor.mleigh@gmail.com>
2020-02-08 09:42:29 -0600

    added all components to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,49 +10,39 @@ export default class App extends React.Component {
     return (
       <Grid
         columns={
-          "1fr minmax(300px, 420px) minmax(200px, 350px) minmax(194px, 250px)"
+          "minmax(30px, 1fr) 600px minmax(240px, 430px) minmax(0px, 1fr) minmax(0px, 350px)"
         }
-        rows={"108px minmax(537px, 1fr)"}
-        justifyContent="space-around"
+        rows={"108px minmax(px, 1fr)"}
+        justifyContent="space-between"
         areas={[
-          "matt-comp matt-comp matt-comp matt-comp",
-          ". tay-comp roy-comp travis-comp",
-          "david-comp david-comp david-comp david-comp",
-          "mark-comp mark-comp mark-comp mark-comp"
+          "matt-comp matt-comp matt-comp matt-comp matt-comp",
+          "tay-comp tay-comp roy-comp . travis-comp",
+          "david-comp david-comp david-comp david-comp david-comp",
+          "mark-comp mark-comp mark-comp mark-comp mark-comp"
         ]}
       >
-        <Cell
-          middle
-          style={{ display: "flex", verticalAlign: "middle" }}
-          area="matt-comp"
-        >
+        <Cell area="matt-comp">
           <div id="app"></div>
         </Cell>
-        <Cell
-          middle
-          style={{ display: "flex", verticalAlign: "middle" }}
-          area="tay-comp"
-        >
-          <div id="tay-prodimage"></div>
+        <Cell middle style={{ display: "flex" }} area="tay-comp">
+          <div
+            id="tay-prodimage"
+            style={{ minWidth: 768, verticalAlign: "top", alignSelf: "top" }}
+          ></div>
         </Cell>
-        <Cell
-          middle
-          style={{ display: "flex", verticalAlign: "middle" }}
-          area="roy-comp"
-        >
+        <Cell middle style={{ display: "flex" }} area="roy-comp">
           <div
             id="productDescription"
             className="d-none d-sm-block"
+            style={{ marginLeft: "35px" }}
           ></div>
-         
         </Cell>
         <Cell
           middle
           style={{ display: "flex", verticalAlign: "middle" }}
           area="travis-comp"
         >
-          {" "}
-          <div className="d-none d-sm-block"></div>
+          <div id="appTravis" className="d-none d-sm-block"></div>
         </Cell>
         <Cell
           middle
